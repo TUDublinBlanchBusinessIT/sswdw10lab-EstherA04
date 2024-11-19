c95ae2985abdb2bcfa7aaaa36caf63f8e0b8ea34
unknown <estherakinlade04@gmail.com>
2024-11-19 15:28:06 +0000

    part 3 working

--- dbcon.php
+++ dbcon.php
@@ -5,12 +5,9 @@ $password = "";
 $dbname = "bookflight";
 $port = 3306;
 
-// Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname, $port);
 
-// Check connection
 if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
+    die("Connection failed: " . mysqli_connect_error());
 }
-
 ?>
